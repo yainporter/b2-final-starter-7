fc040915360fbbc0c018bf580e4a4841cb7c6c64
Michael Dao <mike@turing.edu>
2023-02-28 11:10:23 -0700

    adds models and model specs

---
+++ app/models/invoice.rb
@@ -0,0 +1,16 @@
+class Invoice < ApplicationRecord
+  validates_presence_of :status,
+                        :customer_id
+
+  belongs_to :customer
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+  has_many :merchants, through: :items
+
+  enum status: [:cancelled, :in_progress, :completed]
+
+  def total_revenue
+    invoice_items.sum("unit_price * quantity")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :merchant, only: [:show] do
+    resources :dashboard, only: [:index]
+    resources :items, except: [:destroy]
+    resources :item_status, only: [:update]
+    resources :invoices, only: [:index, :show, :update]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :admin do
+    resources :dashboard, only: [:index]
+    resources :merchants, except: [:destroy]
+    resources :merchant_status, only: [:update]
+    resources :invoices, except: [:new, :destroy]
+  end
 end
