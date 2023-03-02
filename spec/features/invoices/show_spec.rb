3ba8a55bfe52b6f0e719a3be9c72e4b0a42f53fc
Michael Dao <mike@turing.edu>
2023-03-02 08:53:33 -0700

    adds views

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'invoices show' do
+RSpec.describe "invoices show" do
   before :each do
-    @merchant1 = Merchant.create!(name: 'Hair Care')
-    @merchant2 = Merchant.create!(name: 'Jewelry')
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    @merchant2 = Merchant.create!(name: "Jewelry")
 
     @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
     @item_2 = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: @merchant1.id)
@@ -15,12 +15,12 @@ RSpec.describe 'invoices show' do
     @item_5 = Item.create!(name: "Bracelet", description: "Wrist bling", unit_price: 200, merchant_id: @merchant2.id)
     @item_6 = Item.create!(name: "Necklace", description: "Neck bling", unit_price: 300, merchant_id: @merchant2.id)
 
-    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
-    @customer_2 = Customer.create!(first_name: 'Cecilia', last_name: 'Jones')
-    @customer_3 = Customer.create!(first_name: 'Mariah', last_name: 'Carrey')
-    @customer_4 = Customer.create!(first_name: 'Leigh Ann', last_name: 'Bron')
-    @customer_5 = Customer.create!(first_name: 'Sylvester', last_name: 'Nader')
-    @customer_6 = Customer.create!(first_name: 'Herber', last_name: 'Kuhn')
+    @customer_1 = Customer.create!(first_name: "Joey", last_name: "Smith")
+    @customer_2 = Customer.create!(first_name: "Cecilia", last_name: "Jones")
+    @customer_3 = Customer.create!(first_name: "Mariah", last_name: "Carrey")
+    @customer_4 = Customer.create!(first_name: "Leigh Ann", last_name: "Bron")
+    @customer_5 = Customer.create!(first_name: "Sylvester", last_name: "Nader")
+    @customer_6 = Customer.create!(first_name: "Herber", last_name: "Kuhn")
 
     @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
     @invoice_2 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-28 14:54:09")
