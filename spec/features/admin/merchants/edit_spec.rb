2c76177ab37afda99a0be68b110efe6b4337be7e
Michael Dao <mike@turing.edu>
2023-03-02 08:35:34 -0700

    adds feature tests

---
+++ spec/features/admin/merchants/edit_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+describe "Admin Merchant Show" do
+  before :each do
+    @m1 = Merchant.create!(name: "Merchant 1")
+    visit edit_admin_merchant_path(@m1)
+  end
+
+  it "should have a form that redirects back to admin merchant show with a flash message" do
+    fill_in "merchant[name]", with: "Dang Boiii"
+    click_button
+
+    expect(current_path).to eq(admin_merchant_path(@m1))
+    expect(page).to have_content("Dang Boiii")
+    expect(page).to have_content("Merchant Has Been Updated!")
+  end
+
+  it "shows a flash message if not all sections are filled in" do
+    fill_in "Name", with: ""
+
+    click_button
+
+    expect(current_path).to eq(edit_admin_merchant_path(@m1))
+    expect(page).to have_content("All fields must be completed, get your act together.")
+  end
+end
---
+++ spec/features/admin/merchants/new_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+describe "Admin Merchant New" do
+  before :each do
+    @m1 = Merchant.create!(name: "Merchant 1")
+  end
+  
+  it "should be able to fill in a form and create a new merchant" do
+    visit new_admin_merchant_path
+
+    fill_in :name, with: "Dingley Doo"
+
+    click_button
+
+    expect(current_path).to eq(admin_merchants_path)
+    expect(page).to have_content("Dingley Doo")
+    expect(page).to have_content("Merchant Has Been Created!")
+  end
+end
